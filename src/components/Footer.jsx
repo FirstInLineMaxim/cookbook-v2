cb07608f25bdce780a58c96e7d8b09d50f655deb
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-02 22:13:49 +0100

    added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+<nav>
+    <li>About</li>
+    <li>Contact</li>
+    <li>TheDev's</li>
+    <li>History</li>
+    <li>AGB</li>
+    <li>Policy</li>
+</nav>
+  )
+}
