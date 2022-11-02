bec4a849bf4b2cab303b2f6f48df726ac017d056
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-02 22:31:38 +0100

    adding footer stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-<nav>
-    <li>About</li>
-    <li>Contact</li>
-    <li>TheDev's</li>
-    <li>History</li>
-    <li>AGB</li>
-    <li>Policy</li>
-</nav>
+    <>
+    <div className='footer-container'>
+        <nav className='nav-container'>
+            <a href='#' >About</a>
+            <a href='#' >Contact</a>
+            <a href='#' >TheDev's</a>
+            <a href='#' >History</a>
+            <a href='#' >AGB</a>
+            <a href='#' >Policy</a>
+        </nav>
+        <div className='contacts-container'>
+            <div className='socials'>
+                <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/344/external-placeholder-agriculture-and-farmer-kiranshastry-gradient-kiranshastry.png" alt="Instagram" />
+                <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/344/external-placeholder-agriculture-and-farmer-kiranshastry-gradient-kiranshastry.png" alt="Github" />
+                <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/344/external-placeholder-agriculture-and-farmer-kiranshastry-gradient-kiranshastry.png" alt="Facebook" />
+                <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/344/external-placeholder-agriculture-and-farmer-kiranshastry-gradient-kiranshastry.png" alt="LinkedIn" />
+            </div>
+            <input type={'text'}/>
+        </div>
+</div>
+</>
   )
 }
