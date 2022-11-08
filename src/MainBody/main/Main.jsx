bd28968d9720fbe41b8777bb966368c5a0d1851c
Frank Asamoah <frank.asamoah119@gmail.com>
2022-11-08 16:55:36 +0100

    implementing router

---
+++ src/MainBody/main/Main.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import "./Main.css";
+
+import getContentful from "../../getContentful";
+
+import React from "react";
+
+const Main = () => {
+  const [foods, setFoods] = useState(null);
+
+  const { getFoods } = getContentful();
+
+  useEffect(() => {
+    // set the foods here since it 's already an array
+    getFoods()
+      .then((res) => {
+        // console.log(res);
+        setFoods(res);
+      })
+      .catch((err) => console.log(err));
+  }, [getFoods]);
+
+  return (
+    <div className="container">
+      <div className="slider">
+        {foods &&
+          foods.map((food) => (
+            <div key={food.mainImage.file.details.size} className="img-display">
+              <img
+                src={`${food.mainImage.file.url}`}
+                alt={food.mainImage.file.url}
+              className="img"/>
+              <div>
+                <Link to={food.mainImage.title} className="link">{food.title}</Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
+import FoodItems from "./MainBody/item/FoodItems";
+import Main from "./MainBody/main/Main";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="main" element={<Main />} />
+        <Route path="main/:food" element={<FoodItems />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
