1444683d94c5f9830db8320b4c329138ad2d6f4e
Tiefengnom <111353598+Tiefengnom@users.noreply.github.com>
2022-11-10 18:02:44 +0100

    f

--- src/Header/Head.jsx
+++ src/Header/Head.jsx
@@ -23,12 +23,12 @@ function Head () {
         <div className="Header">
     <NavLink to="main" className="nav" >Main</NavLink>
     <NavLink to="/" className="nav" >App</NavLink>
+    <a href="https://www.tasteatlas.com/100-most-popular-foods-in-the-world" className="nav">International Food delicacies</a> 
         {!foodlist ? (<p>Loading</p>) : (
     <select onChange={e => navigate(`main/${e.target.value}`)}>
          <option value="0">Select Food:</option>
      {foodlist.map((food)=> (<option key="{value}" value={food.mainImage.title} >{food.title}</option>))}
-        
-        </select>
+          </select>
     )}
     </div>
     );
